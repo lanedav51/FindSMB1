3bfe8af28bfdc8f88823924910e3a270a9e57166
lanedav51 <lanedav51@gmail.com>
2019-10-01 12:19:24 -0400

    new file:   FindSMB1.ps1 	new file:   ReadMe.txt

---
+++ FindSMB1.ps1
@@ -0,0 +1,24 @@
+$TxtPath = "InsertPathHere"
+$File = Import-Csv $TxtPath
+$Computers = $File.Computers
+
+foreach($Computer in $Computers)
+{
+    Enter-PSSession -ComputerName $Computer
+    $SMB1 = (Get-SmbServerConfiguration).EnableSMB1Protocol
+    $i = 0
+    $obj = new-object psobject -Property @{
+        ComputerName = $Computer
+    }
+    if($SMB1 -eq "True" -AND $i -eq 0)
+    {
+        $obj | Export-Csv -Path SMB1Enabled.csv
+        $i++
+    }
+    elseif ($SMB1 -eq "True" -AND $i -ge 1) 
+    {
+        $obj | Export-Csv -Path SMB1Enabled.csv -Append
+        $i++
+    }
+    Exit-PSSession
+}
---
+++ ReadMe.txt
@@ -0,0 +1,1 @@
+This is a script that finds if SMB1 is active
